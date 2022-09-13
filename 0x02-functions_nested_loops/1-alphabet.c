74e7131e75399da9c17b1d03b6623f30c4a78c4d
Khalidade <khalidadeleke24gmail.com>
2022-09-13 09:02:04 -0700

    betty

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,16 +2,20 @@
 /**
  * main - Entry point
  *
- * Return: Always 0.
+ * Return: Always 0 (success)
  */
+int main(void)
+{
 void print_alphabet(void)
 {
 		char letter;
 
 			for (letter = 'a' ; letter <= 'z'; letter++)
 				_putchar(letter);
+
 				_putchar('\n');
-				
+
+}
 }
 
 
