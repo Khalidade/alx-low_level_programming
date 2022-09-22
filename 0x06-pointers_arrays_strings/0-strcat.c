4018974064be9681a500f28ab47f2cb9cc20bc84
Khalidade <khalidadeleke24gmail.com>
2022-09-21 23:50:47 -0700

    morning

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,12 +9,12 @@
  */
 char *strcat(char *dest, const char *src)
 {
-	int index = 0, dest_len = 0;
+	int i = 0, dest_len = 0;
 
-	while (dest[index++])
+	while (dest[i++])
 		dest_len++;
 
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
+	for (index = 0; src[]; +)
+		dest[dest_len++] = src[i];
 	return (dest);
 }
