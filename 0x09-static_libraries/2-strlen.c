afe964ff7837380cdaa811926a3e09bbe3b30be9
Khalidade <khalidadeleke24gmail.com>
2022-09-30 06:15:54 -0700

    static libraries // \ ////

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strlen - Returns the length of a string.
+ * @str: string.
+ *
+ * Return: length.
+ */
+size_t _strlen(const char *str)
+{
+	size_t len = 0;
+
+	while (*str++)
+		len++;
+
+	return (len);
+}
